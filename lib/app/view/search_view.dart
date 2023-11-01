93692859198dbcc7af8f3174a242343fd8875e79
RojasEnrique <rojas.enrique@proton.me>
2023-11-01 14:34:55 +0100

    Improved search_view.dart UI

--- lib/app/view/search_view.dart
+++ lib/app/view/search_view.dart
@@ -17,20 +17,27 @@ class _SearchViewState extends State<SearchView> {
     var pokemonData = pokemonPreviewRepository.getMockData();
 
     var theme = Theme.of(context);
-    var styleTitle = theme.textTheme.headlineLarge!.copyWith(fontWeight: FontWeight.bold);
+    var styleTitle =
+        theme.textTheme.headlineLarge!.copyWith(fontWeight: FontWeight.bold);
+
+    var dividerIntent = 40.0;
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text('Search', style: styleTitle),
+          SizedBox(height: 16,),
           Row(
             children: [
               const Expanded(child: SizedBox(child: TextField())),
               SizedBox(width: 16),
-              ElevatedButton(onPressed: () {}, child: Icon(Icons.search_rounded))
+              ElevatedButton(
+                  onPressed: () {}, child: Icon(Icons.search_rounded))
             ],
           ),
+          SizedBox(height: 32,),
           Expanded(
               child: ListView.separated(
             itemBuilder: (_, index) {
@@ -39,7 +46,8 @@ class _SearchViewState extends State<SearchView> {
               return PokemonPreviewItem(
                   pokemonPreviewModel: pokemonPreviewModel);
             },
-            separatorBuilder: (_, __) => const SizedBox(height: 0),
+            separatorBuilder: (_, __) =>
+                Divider(indent: dividerIntent, endIndent: dividerIntent),
             itemCount: pokemonData.length,
           )),
           ElevatedButton(
