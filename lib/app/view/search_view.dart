e40b18713cc211ac92d2454c7e1be3f6c83cafd1
RojasEnrique <rojas.enrique@proton.me>
2023-11-01 18:10:33 +0100

    Cleaned search_view.dart

--- lib/app/view/search_view.dart
+++ lib/app/view/search_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_monsters/app/repository/pokemon_preview_repository.dart';
 import 'package:my_monsters/app/view/components/pokemon_preview_item.dart';
-import 'package:my_monsters/app/view/details_view.dart';
 
 class SearchView extends StatefulWidget {
   const SearchView({super.key});
@@ -21,27 +20,29 @@ class _SearchViewState extends State<SearchView> {
         theme.textTheme.headlineLarge!.copyWith(fontWeight: FontWeight.bold);
 
     var dividerIntent = 40.0;
+    var paddingHorizontal = 24.0;
+    var paddingVertical = 8.0;
+    var sizedBoxMedium = 16.0;
+    var sizedBoxLarge = 32.0;
+    var textSearch = "Search";
 
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+      padding: EdgeInsets.symmetric(
+          horizontal: paddingHorizontal, vertical: paddingVertical),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Search', style: styleTitle),
-          SizedBox(
-            height: 16,
-          ),
+          Text(textSearch, style: styleTitle),
+          SizedBox(height: sizedBoxMedium),
           Row(
             children: [
               const Expanded(child: SizedBox(child: TextField())),
-              SizedBox(width: 16),
+              SizedBox(width: sizedBoxMedium),
               ElevatedButton(
-                  onPressed: () {}, child: Icon(Icons.search_rounded))
+                  onPressed: () {}, child: const Icon(Icons.search_rounded))
             ],
           ),
-          SizedBox(
-            height: 32,
-          ),
+          SizedBox(height: sizedBoxLarge),
           Expanded(
               child: ListView.separated(
             itemBuilder: (_, index) {
