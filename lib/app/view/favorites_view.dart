aaf6e5ffc6f717421ed8052cb72e28742b246b8e
RojasEnrique <rojas.enrique@proton.me>
2023-11-01 18:16:28 +0100

    Made favorites_view.dart UI

--- lib/app/view/favorites_view.dart
+++ lib/app/view/favorites_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../repository/pokemon_preview_repository.dart';
+import 'components/pokemon_preview_item.dart';
+
 class FavoritesView extends StatefulWidget {
   const FavoritesView({super.key});
 
@@ -10,11 +13,42 @@ class FavoritesView extends StatefulWidget {
 class _FavoritesViewState extends State<FavoritesView> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('My Favorites'),
-        Expanded(child: ListView(children: [Text('Item1'), Text('Item2')],))
-      ],
+    var pokemonPreviewRepository = PokemonPreviewRepository();
+    var pokemonData = pokemonPreviewRepository.getMockData();
+
+    var theme = Theme.of(context);
+    var styleTitle =
+    theme.textTheme.headlineLarge!.copyWith(fontWeight: FontWeight.bold);
+
+    var dividerIntent = 40.0;
+    var paddingHorizontal = 24.0;
+    var paddingVertical = 8.0;
+    var sizedBoxMedium = 16.0;
+    var sizedBoxLarge = 32.0;
+    var textFavorites = "My Favorites";
+
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          horizontal: paddingHorizontal, vertical: paddingVertical),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(textFavorites, style: styleTitle),
+          SizedBox(height: sizedBoxLarge),
+          Expanded(
+              child: ListView.separated(
+                itemBuilder: (_, index) {
+                  var pokemonPreviewModel = pokemonData[index];
+
+                  return PokemonPreviewItem(
+                      pokemonPreviewModel: pokemonPreviewModel);
+                },
+                separatorBuilder: (_, __) =>
+                    Divider(indent: dividerIntent, endIndent: dividerIntent),
+                itemCount: pokemonData.length,
+              )),
+        ],
+      ),
     );
   }
 }
